df79b87ff5a995afbefc8643821cd34162b45164
Vipul1bhatnagar <vipul.bhatnagar1999@gmail.com>
2018-03-20 14:38:22 +0530

    this is my first commit

---
+++ main.py
@@ -0,0 +1,9 @@
+print("hello world")
+print("what's up?")
+print("let's get started...")
+spy_name=input("whats your name?")
+print("welcome" + spy_name + ". Glad to have you back with us")
+spy_saluation = input("what should we call you(Mr. or Ms.)?")
+#spy_saluation + " " + spy_name
+spy_name = spy_saluation + " " + spy_name
+print(spy_name)
