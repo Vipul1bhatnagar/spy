ad109b0e657d63e1eb3f90c6436377538c98c306
Vipul1bhatnagar <vipul.bhatnagar1999@gmail.com>
2018-04-16 22:14:11 +0530

    seventh commit

--- spy_details.py
+++ spy_details.py
@@ -1,18 +1,14 @@
-
-
-#####name="Deepanshu Lamba"
+#####name="Vipul Bhatnagar"
 #####spy_salutation="Mr."
 #####full_name=spy_salutation + " " + name
 #####spy_age=21
 #####spy_rating=4
 #####status = "You are in..."
 
-#####spy = {"Name": "Deepanshu Lamba", "Salutation": "Mr.", "age": 21, "Rating": 4.5, "Status": "You are in...."}
+#####spy = {"Name": "Vipul Bhatnagar", "Salutation": "Mr.", "age": 21, "Rating": 4.5, "Status": "You are in...."}
 
 #####class for storing details
 from datetime import datetime
-
-
 class Spy:
     def __init__(self, Name, Salutation, age, Rating):
         self.Name = Name
@@ -23,23 +19,21 @@ class Spy:
         self.chats = []
         self.current_status_message = None
 
-
 #####for an existing user
-spy = Spy("Deepanshu Lamba", "Mr.", 21, 4.5)
+spy = Spy("Vipul Bhatnagar", "Mr.", 21, 4.5)
 
 #####existing friends
-friend_one = Spy("Kriti", "Ms.", 21, 4.1)
-friend_two = Spy("Jake", "Mr.", 21, 4.2)
-friend_three = Spy("Ryan", "Mr.", 20, 4)
+#friend_one = Spy("Kriti", "Ms.", 21, 4.1)
+#friend_two = Spy("Jake", "Mr.", 21, 4.2)
+#friend_three = Spy("Ryan", "Mr.", 20, 4)
 
-Friends = [friend_one, friend_two, friend_three]
-
+#Friends = [friend_one, friend_two, friend_three]
 
 #####chat class
 class ChatMessage:
-    def __init__(self, message, sent_by_me):
+    def __init__(self, spy_name, friend_name, message, time, sent_by_me):
+        self.spy_name = spy_name
+        self.friend_name = friend_name
         self.message = message
-        self.time = datetime.now()
+        self.time = time
         self.sent_by_me = sent_by_me
-
-
